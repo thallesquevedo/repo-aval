49a3c8664d009fbcb7dadae55b560e5b0871ef73
Thalles Quevedo <86449143+thallesquevedo@users.noreply.github.com>
2023-04-17 13:12:52 -0300

    Revert "Feature/mult"

--- calculator.js
+++ calculator.js
@@ -12,9 +12,7 @@ function div(v1, v2) {
 }
 
 // retorna o valor da multiplicação entre os dois parâmetros
-function mult(v1, v2) {
-  return v1 * v2
-}
+function mult(v1, v2) {}
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 function square(v1) {}
